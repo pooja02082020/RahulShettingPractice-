eca3e387f94eb428eaaaf99e91aed4ccace74596
pooja02082020 <lohanipooja77@gmail.com>
2024-05-16 18:09:48 -0400

    log in

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         System.setProperty("webdriver.chrome,driver","");
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/loginpagePractise/");
